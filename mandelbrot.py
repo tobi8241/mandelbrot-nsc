eedc16c66fa551dab99125491196feee5ed380aa
Tobias Jagd <kd60an@student.aau.dk>
2026-02-05 14:25:49 +0100

    commit test

---
+++ mandelbrot.py
@@ -0,0 +1,20 @@
+"""
+Mandelbrot Set Generator
+Author : [ Your Name ]
+Course : Numerical Scientific Computing 2026
+"""
+def f(x):
+"""
+Example function .
+Parameters
+----------
+x : float
+Input value
+Returns
+-------
+float
+Output value
+"""
+# TODO : Implement the algorithm
+pass
+#commit test
